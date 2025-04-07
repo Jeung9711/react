b0e53be3da01b18a5d5b36adcfca84c9c056f9e4
Jeung9711 <134358261+Jeung9711@users.noreply.github.com>
2025-04-07 22:05:44 +0900

    250407 코드 리뷰 후 수정
    - userData 누적 저장
    - 회원가입 성공 시 로그인 페이지로 연결

--- toy1_board/src/components/Login.jsx
+++ toy1_board/src/components/Login.jsx
@@ -1,66 +1,91 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FormWrapper, Label, Input, SubmitButton, ErrorMessage} from './styles/FormStyles';
-import { LoginTitle } from './styles/LoginStyles'; 
+import {
+  FormWrapper,
+  Label,
+  Input,
+  SubmitButton,
+  ErrorMessage,
+} from "./styles/FormStyles";
+import { LoginTitle } from "./styles/LoginStyles";
 
-const Login = ()=>{
-    const [loginId, setLoginId] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoggingIn, SetisLoggingIn] = useState(false);
-    const [loginError, setLoginError] = useState('');
-    const navigate = useNavigate();
-    
-    const handleLoginSubmit = async (e) => {
-        e.preventDefault();
+const Login = () => {
+  const [loginId, setLoginId] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggingIn, SetisLoggingIn] = useState(false);
+  const [loginError, setLoginError] = useState("");
+  const navigate = useNavigate();
 
-        if(isLoggingIn) return;
-        SetisLoggingIn(true);
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
 
-        // 로그인
-        const storeUserData = JSON.parse(localStorage.getItem('userData'));
+    if (isLoggingIn) return;
+    SetisLoggingIn(true);
 
-        if(!storeUserData) {
-            setLoginError('회원 정보가 존재하지 않습니다.');
-            SetisLoggingIn(false);
+    // 로그인
+    const storeUserData = JSON.parse(localStorage.getItem("userData"));
+    const checkUser = storeUserData.find((user) => {
+      return (
+        (loginId === user.username || loginId === user.email) &&
+        password === user.password
+      );
+    });
 
-            return;
-        }
+    if (!storeUserData) {
+      setLoginError("회원 정보가 존재하지 않습니다.");
+      SetisLoggingIn(false);
 
-        if(
-            (loginId === storeUserData.username || loginId === storeUserData.email) &&
-            (password === storeUserData.password)
-        ) {
-            console.log("로그인 성공");
-            navigate('/landing-page');
-        } else {
-            setLoginError('아이디 또는 비밀번호가 틀렸습니다.');
-        }
-        
-        SetisLoggingIn(false);
+      return;
     }
 
-    return(
-        <FormWrapper>
-            <LoginTitle>로그인</LoginTitle>
-            <form onSubmit={handleLoginSubmit} method="">
-                <Label htmlFor="login_id">아이디</Label>
-                <Input 
-                    className="login_id" type="text"  id="login_id" name="id"
-                    value={loginId} placeholder="아이디 또는 이메일을 입력하세요"
-                    onChange={(e) => setLoginId(e.target.value)}></Input>
-                <Label htmlFor="login_pw">비밀번호</Label>
-                <Input
-                    className="login_pw" type="password"  id="login_pw" name="password"
-                    value={password} placeholder="비밀번호를 입력하세요"
-                    onChange={(e)=> {setPassword(e.target.value)}}></Input>
-                <SubmitButton type="submit" onClick={handleLoginSubmit} disabled={isLoggingIn}>
-                    {isLoggingIn? '로그인 중...' : '로그인'}
-                </SubmitButton>
-                {loginError && <ErrorMessage>{loginError}</ErrorMessage>}
-            </form>
-            <Link to='/registration-page'>아직 계정이 없으신가요? 회원가입</Link>
-        </FormWrapper>
-    );
-}
+    if (checkUser) {
+      console.log("로그인 성공");
+      navigate("/landing-page");
+    } else {
+      setLoginError("아이디 또는 비밀번호가 틀렸습니다.");
+    }
+
+    SetisLoggingIn(false);
+  };
+
+  return (
+    <FormWrapper>
+      <LoginTitle>로그인</LoginTitle>
+      <form onSubmit={handleLoginSubmit} method="">
+        <Label htmlFor="login_id">아이디</Label>
+        <Input
+          className="login_id"
+          type="text"
+          id="login_id"
+          name="id"
+          value={loginId}
+          placeholder="아이디 또는 이메일을 입력하세요"
+          onChange={(e) => setLoginId(e.target.value)}
+        ></Input>
+        <Label htmlFor="login_pw">비밀번호</Label>
+        <Input
+          className="login_pw"
+          type="password"
+          id="login_pw"
+          name="password"
+          value={password}
+          placeholder="비밀번호를 입력하세요"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></Input>
+        <SubmitButton
+          type="submit"
+          onClick={handleLoginSubmit}
+          disabled={isLoggingIn}
+        >
+          {isLoggingIn ? "로그인 중..." : "로그인"}
+        </SubmitButton>
+        {loginError && <ErrorMessage>{loginError}</ErrorMessage>}
+      </form>
+      <Link to="/registration-page">아직 계정이 없으신가요? 회원가입</Link>
+    </FormWrapper>
+  );
+};
 
 export default Login;
--- toy1_board/src/components/Registration.jsx
+++ toy1_board/src/components/Registration.jsx
@@ -1,162 +1,238 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { FormWrapper, Label, Input, SubmitButton, ErrorMessage, SuccessMessage } from "./styles/FormStyles";
-import { RegisterTitle } from './styles/RegisterStyles';
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  FormWrapper,
+  Label,
+  Input,
+  SubmitButton,
+  ErrorMessage,
+  SuccessMessage,
+} from "./styles/FormStyles";
+import { RegisterTitle } from "./styles/RegisterStyles";
 
-const Registration = () =>{
-    const [signData, setSignData] = useState({
-        username:'',
-        email: '',
-        password: '',
-        passwordConfirm:'',
-        nickname: ''
+const Registration = () => {
+  const [signData, setSignData] = useState([]);
+  const [currentData, setCurrentData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    nickname: "",
+  });
+  const [isSigningUp, setIsSigningUp] = useState(false);
+  const [usernameError, setUsernameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [passwordConfirmError, setPasswordConfirmError] = useState("");
+  const [signupError, setSignupError] = useState();
+  const [signupSuccess, setSignupSuccess] = useState();
+  const navigator = useNavigate();
+
+  // 기존의 로컬 스토리지에 있던 userData를 저장
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem("userData"));
+    if (saved) setSignData(saved);
+  }, []);
+
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+
+    setCurrentData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    if (name == "password") {
+      checkPassword(value);
+    }
+  };
+
+  // 각 변수에 대한 유효성 검사
+  const checkUsername = async (value) => {
+    const storeUserData = JSON.parse(localStorage.getItem("userData"));
+
+    if (storeUserData && storeUserData.username === value) {
+      setUsernameError("이미 사용중인 아이디입니다.");
+    } else {
+      setUsernameError("");
+    }
+  };
+
+  const checkEmail = (value) => {
+    if (!/@/.test(value)) {
+      setEmailError("유효하지 않은 이메일 형식입니다.");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const checkPassword = (value) => {
+    if (value.length < 8) {
+      setPasswordError("비밀번호는 최소 8자 이상이어야 합니다.");
+    } else if (!/[A-Z]/.test(value)) {
+      setPasswordError("비밀번호는 대문자를 최소 1개 포함해야 합니다.");
+    } else if (!/[!@#$%^&*]/.test(value)) {
+      setPasswordError("비밀번호는 특수문자를 최소 1개 포함해야 합니다.");
+    } else if (!/[0-9]/.test(value)) {
+      setPasswordError("비밀번호는 숫자를 최소 1개 포함해야 합니다.");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  const checkPasswordConfirmError = async (value) => {
+    const password = currentData.password;
+
+    if (password != value) {
+      setPasswordConfirmError("비밀번호가 일치하지 않습니다.");
+      console.log("password:", password, "passwordConfirm:", value);
+    } else {
+      setPasswordConfirmError("");
+    }
+  };
+
+  // 제출에 대한 동작 코드
+  const handleSignupSubmit = async (e) => {
+    e.preventDefault();
+
+    if (isSigningUp) return; //중복 클릭 방지
+    setIsSigningUp(true);
+    setSignupError("");
+    setSignupSuccess("");
+
+    // 유효성 검사
+    await checkUsername(currentData.username);
+    checkPassword(currentData.password);
+    checkEmail(currentData.email);
+    await checkPasswordConfirmError(currentData.passwordConfirm);
+
+    // currentData 저장
+    // setState가 비동기 처리되어 빈 배열이 업로드, 이를 막기위해 변수로 저장
+    const updateData = [...signData, currentData];
+    setSignData(updateData);
+    setCurrentData({
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      nickname: "",
     });
-    const [isSigningUp, setIsSigningUp] = useState(false);
-    const [usernameError, setUsernameError] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-    const [passwordConfirmError, setPasswordConfirmError] = useState('');
-    const [signupError, setSignupError] = useState();
-    const [signupSuccess, setSignupSuccess] = useState();
 
-    const handleChange= async (e) => {
-        const {name, value} = e.target;
-
-        setSignData((prev) => ({
-            ...prev,
-            [name]: value
-        }))
-
-        if(name == 'password') {
-            checkPassword(value);
-        }
-    };
-
-    // 각 변수에 대한 유효성 검사
-    const checkUsername = async (value) => {
-        const storeUserData = JSON.parse(localStorage.getItem('userData'));
-
-        if(storeUserData && storeUserData.username === value) {
-            setUsernameError("이미 사용중인 아이디입니다.");
-        } else {
-            setUsernameError("");
-        }
+    // 오류 발생 시 중단
+    if (
+      usernameError !== "" ||
+      emailError !== "" ||
+      passwordError !== "" ||
+      passwordConfirmError !== ""
+    ) {
+      // setSignupError('입력한 정보를 다시 확인해주세요.');
+      setIsSigningUp(false);
     }
 
-    const checkEmail = (value) => {
-        if(!/@/.test(value)){
-            setEmailError('유효하지 않은 이메일 형식입니다.');
-        }else {
-            setEmailError('');
-        }
+    // 로컬 스토리지에 저장
+    try {
+      localStorage.setItem("userData", JSON.stringify(updateData));
+      setSignupSuccess("회원가입이 완료되었습니다!");
+      console.log("가입성공", updateData);
+      // 회원가입에서 로그인 페이지로 연결
+      setTimeout(() => {
+        navigator("/login-page");
+      }, 2000);
+    } catch (error) {
+      setSignupError("저장 중 오류가 발생하였습니다.");
+      console.log("가입 실패");
     }
 
-    const checkPassword = (value) => {
-        if(value.length < 8){
-            setPasswordError('비밀번호는 최소 8자 이상이어야 합니다.');
-        } else if(!/[A-Z]/.test(value)){
-            setPasswordError('비밀번호는 대문자를 최소 1개 포함해야 합니다.');
-        } else if(!/[!@#$%^&*]/.test(value)) {
-            setPasswordError('비밀번호는 특수문자를 최소 1개 포함해야 합니다.');
-        } else if(!/[0-9]/.test(value)) {
-            setPasswordError('비밀번호는 숫자를 최소 1개 포함해야 합니다.');
-        } else {
-            setPasswordError('');
-        }
-    }
+    setIsSigningUp(false);
+  };
 
-    const checkPasswordConfirmError = async (value) => {
-        const password = signData.password;
+  return (
+    <FormWrapper>
+      <RegisterTitle>회원가입</RegisterTitle>
+      <form onSubmit={handleSignupSubmit} method="POST">
+        <Label htmlFor="sign_id">아이디</Label>
+        <Input
+          className="sign_id"
+          type="text"
+          id="sign_id"
+          name="username"
+          value={signData.username}
+          placeholder="사용할 아이디를 입력하세요"
+          onChange={handleChange}
+          onBlur={checkUsername}
+        ></Input>
+        {usernameError != "" ? (
+          <ErrorMessage>{usernameError}</ErrorMessage>
+        ) : null}
 
-        if(password != value) {
-            setPasswordConfirmError('비밀번호가 일치하지 않습니다.');
-            console.log('password:',password,'passwordConfirm:',value);
-        }else {
-            setPasswordConfirmError('');
-        }
-    }
+        <Label htmlFor="sign_email">이메일</Label>
+        <Input
+          className="sign_email"
+          type="email"
+          id="sign_email"
+          name="email"
+          value={signData.email}
+          placeholder="이메일을 입력하세요"
+          onChange={handleChange}
+          onBlur={(e) => checkEmail(e.target.value)}
+        ></Input>
+        {emailError != "" ? <ErrorMessage>{emailError}</ErrorMessage> : null}
 
-    // 제출에 대한 동작 코드
-    const handleSignupSubmit = async (e) => {
-        e.preventDefault();
+        <Label htmlFor="sign_pw">비밀번호</Label>
+        <Input
+          className="sign_pw"
+          type="password"
+          id="sign_pw"
+          name="password"
+          value={signData.password}
+          placeholder="비밀번호를 입력하세요"
+          onChange={handleChange}
+        ></Input>
+        {passwordError != "" ? (
+          <ErrorMessage>{passwordError}</ErrorMessage>
+        ) : null}
 
-        if (isSigningUp) return; //중복 클릭 방지
-        setIsSigningUp(true);
-        setSignupError('');
-        setSignupSuccess('');
+        <Label htmlFor="sign_pw_check">비밀번호 확인</Label>
+        <Input
+          className="sign_pw_check"
+          type="password"
+          id="sign_pw_check"
+          name="passwordConfirm"
+          value={signData.passwordConfirm}
+          placeholder="비밀번호를 다시 입력하세요"
+          onChange={handleChange}
+          onBlur={(e) => {
+            checkPasswordConfirmError(e.target.value);
+          }}
+        ></Input>
+        {passwordConfirmError != "" ? (
+          <ErrorMessage>{passwordConfirmError}</ErrorMessage>
+        ) : null}
 
-        // 유효성 검사
-        await checkUsername(signData.username);
-        checkPassword(signData.password);
-        checkEmail(signData.email);
-        await checkPasswordConfirmError(signData.passwordConfirm);
+        <Label htmlFor="sign_nickname">닉네임</Label>
+        <Input
+          className="sign_nickname"
+          type="text"
+          id="sign_nickname"
+          name="nickname"
+          value={signData.nickname}
+          placeholder="표시할 닉네임을 입력하세요"
+          onChange={handleChange}
+        ></Input>
 
-        // 오류 발생 시 중단
-        if(
-            usernameError !== '' ||
-            emailError !== '' ||
-            passwordError !== '' ||
-            passwordConfirmError !== ''
-        ) {
-            // setSignupError('입력한 정보를 다시 확인해주세요.');
-            setIsSigningUp(false);
-        }
-
-        // 로컬 스토리지에 저장
-        try {
-            localStorage.setItem('userData',JSON.stringify(signData));
-            setSignupSuccess('회원가입이 완료되었습니다!');
-            console.log("가입성공",signData);
-        }catch (error) {
-            setSignupError('저장 중 오류가 발생하였습니다.');
-            console.log('가입 실패');
-        }
-
-        setIsSigningUp(false);
-    }
-
-    return (
-        <FormWrapper>
-            <RegisterTitle>회원가입</RegisterTitle>
-            <form onSubmit={handleSignupSubmit} method="POST" >
-                <Label htmlFor="sign_id">아이디</Label>
-                <Input className='sign_id' type="text" id="sign_id" name="username"
-                value={signData.username} placeholder="사용할 아이디를 입력하세요"
-                onChange={handleChange} onBlur={checkUsername}></Input>
-                { usernameError != ''? <ErrorMessage>{usernameError}</ErrorMessage> : null}
-
-                <Label htmlFor="sign_email">이메일</Label>
-                <Input className="sign_email" type="email" id="sign_email" name="email"
-                value={signData.email} placeholder="이메일을 입력하세요"
-                onChange={handleChange} onBlur={(e) => checkEmail(e.target.value)}></Input>
-                {emailError != ''? <ErrorMessage>{emailError}</ErrorMessage>: null}
-
-                <Label htmlFor="sign_pw">비밀번호</Label>
-                <Input className="sign_pw" type="password" id="sign_pw" name="password"
-                value={signData.password} placeholder="비밀번호를 입력하세요"
-                onChange={handleChange}></Input>
-                {passwordError !=''? <ErrorMessage>{passwordError}</ErrorMessage> : null}
-
-                <Label htmlFor="sign_pw_check">비밀번호 확인</Label>
-                <Input className="sign_pw_check" type="password" id="sign_pw_check" name="passwordConfirm"
-                value={signData.passwordConfirm} placeholder="비밀번호를 다시 입력하세요"
-                onChange={handleChange} onBlur={(e)=>{checkPasswordConfirmError(e.target.value)}}></Input>
-                {passwordConfirmError != ''? <ErrorMessage>{passwordConfirmError}</ErrorMessage> : null}
-
-                <Label htmlFor="sign_nickname">닉네임</Label>
-                <Input className="sign_nickname" type="text" id="sign_nickname" name="nickname"
-                value={signData.nickname} placeholder="표시할 닉네임을 입력하세요"
-                onChange={handleChange}></Input>
-
-                <SubmitButton type='submit' onClick={handleSignupSubmit} disabled={isSigningUp}>
-                    {isSigningUp? '가입 중...' : '회원가입'}
-                </SubmitButton>
-            </form>
-            <Link to="/login-page">이미 계정이 있으신가요? 로그인</Link>
-            {signupError && <ErrorMessage>{signupError}</ErrorMessage>}
-            {signupSuccess && <SuccessMessage>{signupSuccess}</SuccessMessage>}
-        </FormWrapper>
-    );
+        <SubmitButton
+          type="submit"
+          onClick={handleSignupSubmit}
+          disabled={isSigningUp}
+        >
+          {isSigningUp ? "가입 중..." : "회원가입"}
+        </SubmitButton>
+      </form>
+      <Link to="/login-page">이미 계정이 있으신가요? 로그인</Link>
+      {signupError && <ErrorMessage>{signupError}</ErrorMessage>}
+      {signupSuccess && <SuccessMessage>{signupSuccess}</SuccessMessage>}
+    </FormWrapper>
+  );
 };
 
 export default Registration;
